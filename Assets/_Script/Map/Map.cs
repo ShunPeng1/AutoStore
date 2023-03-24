9a1b0438f665d9089ec2602f265bd913f6355323
ShunPeng1 <shunpeng1@gmail.com>
2023-03-24 15:21:22 +0700

    test: Grid

---
+++ Assets/_Script/Map/Map.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    void Start()
+    {
+        Grid grid = new Grid();
+    }
+
+    
+}
