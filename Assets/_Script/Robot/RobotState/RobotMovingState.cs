b3b904e835aa4f0589b31173ec617b1094075fc7
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2023-08-14 22:15:07 +0700

    test: robot moving state

--- Assets/_Script/Robot/RobotState/RobotMovingState.cs
+++ Assets/_Script/Robot/RobotState/RobotMovingState.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Shun_Grid_System;
 using Shun_State_Machine;
 using UnityEngine;
 
@@ -11,14 +13,11 @@ namespace _Script.Robot
         {
             protected RobotTask CurrentTask;
             
-            public RobotMovingState(Robot robot, RobotStateEnum myStateEnum,
-                Action<RobotStateEnum, IStateParameter> executeEvents = null,
-                Action<RobotStateEnum, IStateParameter> exitEvents = null,
-                Action<RobotStateEnum, IStateParameter> enterEvents = null) : base(robot, myStateEnum, executeEvents,
-                exitEvents, enterEvents)
+            
+            public RobotMovingState(Robot robot, RobotStateEnum myStateEnum, Action<RobotStateEnum, IStateParameter> executeEvents = null, Action<RobotStateEnum, IStateParameter> exitEvents = null, Action<RobotStateEnum, IStateParameter> enterEvents = null) : base(robot, myStateEnum, executeEvents, exitEvents, enterEvents)
             {
                 enterEvents += AssignTask;
-                //executeEvents += MovingStateExecute;
+                executeEvents += MovingStateExecute;
             }
 
             private void AssignTask(RobotStateEnum lastRobotState, IStateParameter enterParameters)
@@ -31,26 +30,26 @@ namespace _Script.Robot
                 switch (CurrentTask.StartCellPosition)
                 {
                     case RobotTask.StartPosition.LastCell:
-                        Robot.CreateInitialPath(Robot.LastCellPosition, CurrentTask.GoalCellPosition);
+                        CreateInitialPath(Robot.LastCellPosition, CurrentTask.GoalCellPosition);
                         ExtractNextCellInPath();
                         break;
 
                     case RobotTask.StartPosition.NextCell:
-                        Robot.CreateInitialPath(Robot.NextCellPosition, CurrentTask.GoalCellPosition);
+                        CreateInitialPath(Robot.NextCellPosition, CurrentTask.GoalCellPosition);
                         Robot.MovingPath.RemoveFirst();
                         break;
 
                     case RobotTask.StartPosition.NearestCell:
-                        Vector3 nearestCellPosition = Robot.CurrentGrid.GetWorldPositionOfNearestCell(Robot.transform.position);
+                        Vector3 nearestCellPosition = Grid.GetWorldPositionOfNearestCell(RobotTransform.position);
 
                         if (nearestCellPosition == Robot.LastCellPosition)
                         {
-                            Robot.CreateInitialPath(nearestCellPosition, CurrentTask.GoalCellPosition);
+                            CreateInitialPath(nearestCellPosition, CurrentTask.GoalCellPosition);
                             ExtractNextCellInPath();
                         }
                         else if (nearestCellPosition == Robot.NextCellPosition)
                         {
-                            if (Robot.CreateInitialPath(Robot.NextCellPosition, CurrentTask.GoalCellPosition))
+                            if (CreateInitialPath(Robot.NextCellPosition, CurrentTask.GoalCellPosition))
                                 Robot.MovingPath.RemoveFirst();
                         }
                         else
@@ -64,29 +63,169 @@ namespace _Script.Robot
                         throw new ArgumentOutOfRangeException();
                 }
 
-                CheckArriveCell();
+                CheckArriveGoalCell();
+            }
+            
+            
+            private void MovingStateExecute(RobotStateEnum currentState, IStateParameter enterParameters)
+            {
+                if (!CheckArriveNextCell())
+                {
+                    Robot.IsMidwayMove = false;
+                    ExtractNextCellInPath();
+                } 
+                
+                if (!CheckArriveGoalCell()) return; // change state during executing this function
+            
+                Robot.DetectNearByRobot();
+                if (!DecideFromRobotDetection()) return; // change state during executing this function
+                
+                Robot.IsMidwayMove = true;
+                Robot.MoveAlongGrid();
             }
 
-            bool CheckBetween2Cell()
+            bool CheckArriveNextCell()
             {
-                Robot.IsBetween2Cells = Vector3.Distance(Robot.transform.position, Robot.NextCellPosition) != 0;
-                return Robot.IsBetween2Cells;
+                return Vector3.Distance(RobotTransform.position, Robot.NextCellPosition) != 0;
             }
             
-            bool CheckArriveCell()
+            bool CheckArriveGoalCell()
             {
-                if (CurrentTask != null &&
-                    Robot.CurrentGrid.GetIndex(Robot.transform.position) == Robot.CurrentGrid.GetIndex(CurrentTask.GoalCellPosition))
+                if (! RobotUtility.CheckRobotOnGoal(Robot, CurrentTask)) return true;
+                
+                CurrentTask.GoalArrivalAction?.Invoke();
+                return false;
+
+            }
+
+            #region DETECTION_DECISION
+            private enum DetectDecision
+            {
+                Ignore = 0,
+                Wait = 1,
+                Dodge = 2,
+                Deflected = 3
+            }
+
+            protected bool DecideFromRobotDetection()
+            {
+                List<GridXZCell<CellItem>> dynamicObstacle = new();
+                DetectDecision finalDecision = DetectDecision.Ignore; 
+                
+                foreach (var detectedRobot in Robot.NearbyRobots)
                 {
-                    CurrentTask.GoalArrivalAction?.Invoke();
-                    ExtractNextCellInPath();
-                    return false;
+                    
+                    DetectDecision decision = DecideOnDetectedRobot(detectedRobot);
+                    finalDecision = (DetectDecision) Mathf.Max((int)decision, (int)finalDecision);
+
+                    dynamicObstacle.Add(Grid.GetCell(detectedRobot.LastCellPosition));
+                    if(detectedRobot.IsMidwayMove) dynamicObstacle.Add(Grid.GetCell(detectedRobot.NextCellPosition));
                 }
                 
-                ExtractNextCellInPath();
-                return true;
+                switch (finalDecision)
+                {
+                    case DetectDecision.Ignore:
+                        return true;
+                    
+                    case DetectDecision.Wait: // We set the robot to jam state
+                        //Debug.Log(gameObject.name +" Jam! ");
+                        Robot.SetToJam();
+                        return false;
+                    
+                    case DetectDecision.Dodge: // We add the detected robot cell as obstacle
+                        //Debug.Log(gameObject.name +" Dodge ");
+                        return UpdateInitialPath(dynamicObstacle); // Update Path base on dynamic obstacle
+                     
+                    case DetectDecision.Deflected:
+                        return false;
+                    
+                    default:
+                        return true;
+                }
+            }
+
+            private DetectDecision DecideOnDetectedRobot(Robot detectedRobot)
+            {
+                float dotProductOf2RobotDirection = RobotUtility.DotOf2RobotMovingDirection(Robot, detectedRobot);
+                bool isUnsafeDistanceOf2Robot = Robot.CheckRobotSafeDistance(detectedRobot);
+                bool isBlockAHead = RobotUtility.CheckRobotBlockAHead(detectedRobot, Robot.NextCellPosition);
+                bool isBlockingGoal = RobotUtility.CheckRobotBlockGoal(detectedRobot, CurrentTask);
+                
+                switch (detectedRobot.CurrentRobotState)
+                {
+                    /* Idle state cases */
+                    case RobotStateEnum.Idle when isBlockingGoal || isBlockAHead: // If they are standing on this robot goal or blocking ahead of this robot
+                        return TryRedirectRobot(detectedRobot);
+
+                    case RobotStateEnum.Idle: // Not blocking at all
+                        return DetectDecision.Ignore;
+                    
+                    
+                    /* Jamming state cases */
+                    case RobotStateEnum.Jamming when isBlockAHead: // Currently blocking in between the next cell
+                        //return DetectDecision.Dodge;
+                        return TryRedirectRobot(detectedRobot);
+
+                    case RobotStateEnum.Jamming: //  is not blocking ahead in between the next cell
+                        return DetectDecision.Ignore; 
+                    
+                    
+                    /* Handling states cases */
+                    case RobotStateEnum.Handling when !isBlockAHead: //  is not block ahead
+                        return DetectDecision.Ignore; 
+                    
+                    // Currently blocking in between the next cell , and they are standing on this robot goal
+                    case RobotStateEnum.Handling when isBlockingGoal:
+                        return DetectDecision.Wait;
+                    
+                    case RobotStateEnum.Handling: // Currently blocking in between the next cell, and not on this robot goal
+                        return DetectDecision.Dodge;
+                    
+
+                    /* These are the rest of moving state */
+                    case RobotStateEnum.Delivering:
+                    case RobotStateEnum.Approaching:
+                    case RobotStateEnum.Redirecting:
+                    default:
+                        if (Math.Abs(dotProductOf2RobotDirection - (-1)) < 0.01f ) // opposite direction
+                        {
+                            if(!isBlockAHead) return DetectDecision.Ignore; // same row or column
+                    
+                            // Is block ahead
+                            if (isBlockingGoal) // If they are standing on this robot goal
+                            {
+                                return TryRedirectRobot(detectedRobot);
+
+                            }
+                            else return DetectDecision.Dodge;
+                        }
+
+                        if (Math.Abs(dotProductOf2RobotDirection - 1) < 0.01f && isBlockAHead && isUnsafeDistanceOf2Robot) 
+                            // 2 robot moving same direction and smaller than safe distance
+                        {
+                            //Debug.Log(gameObject.name + " Keep safe distance ahead with "+detectedRobot.gameObject.name);
+                            return DetectDecision.Wait;
+                        }
+                        
+                        if (dotProductOf2RobotDirection == 0) // perpendicular direction
+                        {
+                            return isBlockAHead ? DetectDecision.Wait : DetectDecision.Ignore;
+                        }
+                        
+                        return DetectDecision.Ignore;
+                }
+                
+            }
+
+            private DetectDecision TryRedirectRobot(Robot detectedRobot)
+            {
+                return detectedRobot.RedirectToOrthogonalCell(Robot, Robot.NextCellPosition) ? DetectDecision.Wait : 
+                    Robot.RedirectToOrthogonalCell(detectedRobot, detectedRobot.NextCellPosition) ? DetectDecision.Deflected : DetectDecision.Dodge;
             }
             
+            #endregion
+
+            #region PATH
             
             protected void ExtractNextCellInPath()
             {
@@ -98,15 +237,60 @@ namespace _Script.Robot
                 var nextNextCell = Robot.MovingPath.First.Value;
                 Robot.MovingPath.RemoveFirst(); // the next standing node
 
-                Vector3 nextNextCellPosition = Robot.CurrentGrid.GetWorldPositionOfNearestCell(nextNextCell.XIndex, nextNextCell.YIndex);
+                Vector3 nextNextCellPosition = Grid.GetWorldPositionOfNearestCell(nextNextCell.XIndex, nextNextCell.YIndex);
 
                 Robot.LastCellPosition = Robot.NextCellPosition;
                 Robot.NextCellPosition = nextNextCellPosition;
             
             }
             
-            
+            protected bool CreateInitialPath(Vector3 startPosition, Vector3 endPosition)
+            {
+                var startCell = Grid.GetCell(startPosition);
+                var endCell = Grid.GetCell(endPosition);
+        
+                Robot.MovingPath = Robot.PathfindingAlgorithm.FirstTimeFindPath(startCell, endCell);
 
+                if (Robot.MovingPath != null) return true; 
+        
+                // No destination was found
+                Robot.RedirectToNearestCell();
+
+                return false;
+
+            }
+            
+            protected bool UpdateInitialPath(List<GridXZCell<CellItem>> dynamicObstacle)
+            {
+                Vector3 nearestCellPosition = Grid.GetWorldPositionOfNearestCell(RobotTransform.position);
+                var currentStartCell = Grid.GetCell(nearestCellPosition);
+
+                Robot.MovingPath = Robot.PathfindingAlgorithm.UpdatePathWithDynamicObstacle(currentStartCell, dynamicObstacle);
+
+                if (Robot.MovingPath == null) // The path to goal is block
+                {
+                    Robot.RedirectToNearestCell();
+                    return false;
+                }
+        
+                if (nearestCellPosition == Robot.LastCellPosition)
+                {
+                    ExtractNextCellInPath();
+                    return true;
+                }
+        
+                if (nearestCellPosition == Robot.NextCellPosition)
+                {
+                    Robot.MovingPath.RemoveFirst();
+                    return true;
+                }
+
+                //Debug.LogError( gameObject.name+" THE NEAREST CELL IS NOT LAST OR NEXT CELL "+ nearestCellPosition);
+
+                return false;
+            }
+            
+            #endregion
         }
     }
 }
--- Assets/_Script/Robot/RobotState/RobotState.cs
+++ Assets/_Script/Robot/RobotState/RobotState.cs
@@ -1,16 +1,22 @@
 ﻿using System;
+using Shun_Grid_System;
 using Shun_State_Machine;
+using UnityEngine;
 
 namespace _Script.Robot
 {
     
     public class RobotState : BaseState<RobotStateEnum>
     {
-        protected Robot Robot;
-        
+        protected readonly Robot Robot;
+        protected readonly Transform RobotTransform;
+        protected readonly GridXZ<CellItem> Grid;
+
         public RobotState(Robot robot, RobotStateEnum myStateEnum, Action<RobotStateEnum, IStateParameter> executeEvents = null, Action<RobotStateEnum, IStateParameter> exitEvents = null, Action<RobotStateEnum, IStateParameter> enterEvents = null) : base(myStateEnum, executeEvents, exitEvents, enterEvents)
         {
             Robot = robot;
+            RobotTransform = Robot.transform;
+            Grid = Robot.CurrentGrid;
         }
         
         
