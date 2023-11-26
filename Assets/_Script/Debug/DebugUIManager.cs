dc8bc0754e242b412c14859a9e51be5c337aa687
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2023-11-26 21:18:58 +0700

    feat: timescale

--- Assets/_Script/Debug/DebugUIManager.cs
+++ Assets/_Script/Debug/DebugUIManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using _Script.Managers;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityUtilities;
 
 public class DebugUIManager : SingletonMonoBehaviour<DebugUIManager>
@@ -11,6 +12,11 @@ public class DebugUIManager : SingletonMonoBehaviour<DebugUIManager>
     [SerializeField] private TMP_Text fpsText;
     [SerializeField] private float deltaTime;
     
+    [Header("Time Scale")]
+    [SerializeField] private Slider timeScaleSlider;
+    [SerializeField] private int minTimeScale = 0, maxTimeScale = 100;
+    [SerializeField] private float timeScaleStep = 1.0f;
+    
     [Header("Collision")]
     [SerializeField] private TMP_Text collisionText;
     private int _collosionCount;
@@ -18,7 +24,15 @@ public class DebugUIManager : SingletonMonoBehaviour<DebugUIManager>
     [Header("Crate finish")]
     [SerializeField] private TMP_Text finishText;
     private int _finishCount;
+    
+    void Start()
+    {
+        // Set the initial value of the slider to the current time scale
+        timeScaleSlider.minValue = minTimeScale;
+        timeScaleSlider.maxValue = maxTimeScale;
 
+        timeScaleSlider.value = Time.timeScale;
+    }
     
     void Update () {
         if(!isActive) return;
@@ -26,6 +40,19 @@ public class DebugUIManager : SingletonMonoBehaviour<DebugUIManager>
         deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
         float fps = 1.0f / deltaTime;
         fpsText.text ="FPS " + Mathf.Ceil (fps).ToString ();
+        
+        
+        // Decrease time scale with "J" key
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            AdjustTimeScale(-timeScaleStep);
+        }
+
+        // Increase time scale with "K" key
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            AdjustTimeScale(timeScaleStep);
+        }
     }
 
     public void AddCollision()
@@ -41,5 +68,13 @@ public class DebugUIManager : SingletonMonoBehaviour<DebugUIManager>
         finishText.text = "finish: " + _finishCount;
     }
     
+    private void AdjustTimeScale(float delta)
+    {
+        float newTimeScale = Mathf.Clamp(Time.timeScale + delta, minTimeScale, maxTimeScale);
+        Time.timeScale = newTimeScale;
+        Time.fixedDeltaTime = 0.02f * Time.timeScale; // Adjust fixed delta time accordingly
+        
+        timeScaleSlider.value = newTimeScale;
+    }
     
 }
