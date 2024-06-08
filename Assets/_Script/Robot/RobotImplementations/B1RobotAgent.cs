332177a3487f6754bb9e5b026e974457ce34030a
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2024-06-08 13:09:38 +0700

    temp: robot RL

---
+++ Assets/_Script/Robot/RobotImplementations/B1RobotAgent.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using UnityEngine;
+
+public class B1RobotAgent : Agent
+{
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        base.OnActionReceived(actions);
+        
+        
+    }
+}
